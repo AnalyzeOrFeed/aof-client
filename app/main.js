7b5d25268a14dc58968c084a1cbb048b7cc63523
AndreasGassmann <andreas@andreasgassmann.ch>
2016-05-04 00:30:25 +0200

    chore(build): Change to electron-builder structure

--- app/main.js
+++ app/main.js
@@ -17,7 +17,7 @@ ipc.on("test", () => {
 app.on("ready", () => {
 	mainWindow = new BrowserWindow({ width: 800, height: 600 });
 
-	mainWindow.loadURL("file://" + __dirname + "/app/index.html");
+	mainWindow.loadURL("file://" + __dirname + "/index.html");
 
 	mainWindow.on("closed", () => {
 		mainWindow = null;
